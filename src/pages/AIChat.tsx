89aab5a0e43f2412cfc846b2ea0ad736e10a4364
tangsanyi <1825237337@qq.com>
2026-06-10 16:38:59 +0800

    connect render backend

--- src/pages/AIChat.tsx
+++ src/pages/AIChat.tsx
@@ -29,7 +29,7 @@ export default function AIChat() {
     setInput('')
 
     try {
-      const response = await fetch('http://localhost:3000/chat', {
+      const response = await fetch('https://tangsanyi-blog.onrender.com/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
